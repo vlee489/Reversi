bbd13ed2c3db5b91296e1ca3a1dd0a1e596b7e3e
Vincent Lee <vlee1@live.co.uk>
2018-12-02 23:10:38 +0000

    javadoc comments

--- src/Player.java
+++ src/Player.java
@@ -9,27 +9,33 @@ public class Player {
     private int score;
     private int numOfMoves;
 
+    /**
+     * Constructor method to set the default to 0
+     */
     public Player(){
         score = 0;
         numOfMoves = 0;
     }
 
-    public int getNumOfMoves() {
-        return numOfMoves;
-    }
-
+    /**
+     * get the score
+     * @return the score of object
+     */
     public int getScore() {
         return score;
     }
 
-    public void setNumOfMoves(int numOfMoves) {
-        this.numOfMoves = numOfMoves;
-    }
-
+    /**
+     * sets the score
+     * @param score the score to be entered.
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Increments the moves by one when ran.
+     */
     public void incrementMoves(){
         numOfMoves++;
     }
